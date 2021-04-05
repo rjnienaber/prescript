603c29506d0e64221ec6eb34fba37c9084f089b4
Richard Nienaber <rjnienaber@gmail.com>
2021-04-05 15:32:43 +0100

    write to stdin triggered by last line

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module prescript
 
 go 1.16
 
-require go.uber.org/zap v1.16.0 // indirect
+require go.uber.org/zap v1.16.0
--- main.go
+++ main.go
@@ -7,8 +7,16 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"regexp"
 	"strings"
 )
+
+func processError(err error, logger *zap.SugaredLogger, message string) {
+	if err != nil {
+		logger.Error(message+": ", err)
+		os.Exit(2)
+	}
+}
 
 func createLogger() *zap.Logger {
 	logger, err := zap.NewDevelopment()
@@ -19,35 +27,61 @@ func createLogger() *zap.Logger {
 	return logger
 }
 
-func startCommand(args []string, logger *zap.SugaredLogger) io.ReadCloser {
+func startCommand(args []string, logger *zap.SugaredLogger) (io.ReadCloser, io.WriteCloser, *os.Process) {
 	appArgs := strings.Join(args[1:], ",")
 	cmd := exec.Command(args[0], appArgs)
 	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		logger.Error("Failed to capture stdout: ", err)
-		os.Exit(2)
-	}
+	processError(err, logger, "Failed to capture stdout")
 
 	cmd.Stderr = cmd.Stdout
 
-	if err = cmd.Start(); err != nil {
-		logger.Error("Failed to start app: ", err)
-		os.Exit(2)
-	}
+	stdin, err := cmd.StdinPipe()
+	processError(err, logger, "Failed to capture stdin")
 
-	return stdout
+	err = cmd.Start()
+	processError(err, logger, "Failed to start app")
+
+	return stdout, stdin, cmd.Process
 }
 
 func main() {
-	logger := createLogger()
+	zapLogger := createLogger()
 	// sync errors can be ignored: https://github.com/uber-go/zap/issues/328
-	defer logger.Sync()
-	sugar := logger.Sugar()
+	defer zapLogger.Sync()
+	logger := zapLogger.Sugar()
 
-	stdout := startCommand(os.Args[1:], sugar)
+	stdout, stdin, process := startCommand(os.Args[1:], logger)
 
 	scanner := bufio.NewScanner(stdout)
+	scanner.Split(bufio.ScanBytes)
+	currentLine := ""
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		char := scanner.Text()
+		fmt.Print(char)
+		if char == "\n" {
+			currentLine = ""
+			continue
+		}
+
+		currentLine += char
+		matchLine(currentLine, `HOW MANY ROLLS\? `, "5000", logger, stdin)
+		matchLine(currentLine, `TRY AGAIN\? `, "N", logger, stdin)
+	}
+
+	state, err := process.Wait()
+	processError(err, logger, "Error waiting for process to finish")
+
+	fmt.Println("Exit code: ", state.ExitCode())
+	os.Exit(state.ExitCode())
+}
+
+func matchLine(currentLine string, promptToMatch string, input string, logger *zap.SugaredLogger, stdin io.WriteCloser) {
+	matched, err := regexp.MatchString(promptToMatch, currentLine)
+	processError(err, logger, "Error matching line with regex")
+
+	if matched {
+		fmt.Print(input + "\n")
+		_, err = stdin.Write([]byte(input + "\n"))
+		processError(err, logger, "Error writing to stdin")
 	}
 }
