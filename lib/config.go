fec11e568c8a04819844a3d43f0cc25c6e889bc3
Richard Nienaber <rjnienaber@gmail.com>
2021-05-17 08:38:16 +0100

    added positional arguments

--- lib/config.go
+++ lib/config.go
@@ -13,10 +13,12 @@ const (
 )
 
 type PlayConfig struct {
-	Quiet    bool
-	DontFail bool
-	Verbose  bool
-	Timeout  time.Duration
+	Quiet      bool
+	DontFail   bool
+	Verbose    bool
+	Timeout    time.Duration
+	ScriptFile string
+	Command    string
 }
 
 type Config struct {
@@ -26,22 +28,26 @@ type Config struct {
 
 func createPlaySubCommand(config *Config, defaultTimeout time.Duration) *cobra.Command {
 	var playCmd = &cobra.Command{
-		Use:   "play [optional args] [script file] [executable]",
+		Use:   "play [script file] [optional command]",
 		Short: "runs prescripted commands against an interactive cli",
 		Long:  "runs through a predefined script of responses to an interactive cli",
+		Args:  cobra.RangeArgs(1, 2),
 		Run: func(cmd *cobra.Command, args []string) {
-			// left empty on purpose otherwise default help is displayed
+			config.Play.ScriptFile = args[0]
+			if len(args) > 1 {
+				config.Play.Command = args[1]
+			}
 		},
 	}
 
-	playCmd.PersistentFlags().BoolVarP(&config.Play.Quiet, "quiet", "q", false, "quiet mode, no output")
-	playCmd.PersistentFlags().BoolVarP(&config.Play.DontFail, "dont-fail", "d", false, "dont fail on external command failures")
-	playCmd.PersistentFlags().BoolVarP(&config.Play.Verbose, "verbose", "v", false, "output all logs")
-	playCmd.PersistentFlags().DurationVarP(&config.Play.Timeout, "timeout", "t", defaultTimeout, "timeout waiting for output from external command")
+	playCmd.Flags().BoolVarP(&config.Play.Quiet, "quiet", "q", false, "quiet mode, no output")
+	playCmd.Flags().BoolVarP(&config.Play.DontFail, "dont-fail", "d", false, "dont fail on external command failures")
+	playCmd.Flags().BoolVarP(&config.Play.Verbose, "verbose", "v", false, "output all logs")
+	playCmd.Flags().DurationVarP(&config.Play.Timeout, "timeout", "t", defaultTimeout, "timeout waiting for output from external command")
 	return playCmd
 }
 
-func GetConfig() *Config {
+func GetConfig() (*Config, error) {
 	config := Config{}
 
 	defaultTimeout, _ := time.ParseDuration("30s")
@@ -49,12 +55,16 @@ func GetConfig() *Config {
 	playCmd := createPlaySubCommand(&config, defaultTimeout)
 
 	var rootCmd = &cobra.Command{
-		Use:   "prescript [cmd] [optional args] [script file] [executable]",
+		Use:   "prescript [cmd] [script file] [command]",
 		Short: "record and playback responses to an interactive cli",
 		Long:  "record and playback responses to an interactive cli",
 	}
 	rootCmd.AddCommand(playCmd)
 
-	cobra.CheckErr(rootCmd.Execute())
-	return &config
+	err := rootCmd.Execute()
+	if err != nil {
+		return &Config{}, err
+	}
+
+	return &config, nil
 }
--- lib/config_test.go
+++ lib/config_test.go
@@ -6,40 +6,64 @@ import (
 	"testing"
 )
 
-func testParseArgs(args []string) *Config {
+func testParseArgs(args []string) (*Config, error) {
 	newArgs := append([]string{"prescript"}, args...)
 	os.Args = newArgs
-	config := GetConfig()
-	return config
+	return GetConfig()
 }
 
 func TestConfigPlayQuiet(t *testing.T) {
-	config := testParseArgs([]string{"play", "--quiet"})
+	config, err := testParseArgs([]string{"play", "/tmp/script.json", "--quiet"})
+	assert.NoError(t, err)
 	assert.Equal(t, config.Subcommand, Play)
 	assert.True(t, config.Play.Quiet)
 }
 
 func TestConfigDontFail(t *testing.T) {
-	config := testParseArgs([]string{"play", "--dont-fail"})
+	config, err := testParseArgs([]string{"play", "/tmp/script.json", "--dont-fail"})
+	assert.NoError(t, err)
 	assert.Equal(t, config.Subcommand, Play)
 	assert.True(t, config.Play.DontFail)
 }
 
 func TestConfigVerbose(t *testing.T) {
-	config := testParseArgs([]string{"play", "--verbose"})
+	config, err := testParseArgs([]string{"play", "/tmp/script.json", "--verbose"})
+	assert.NoError(t, err)
 	assert.Equal(t, config.Subcommand, Play)
 	assert.True(t, config.Play.Verbose)
 }
 
 func TestConfigTimeout(t *testing.T) {
-	config := testParseArgs([]string{"play", "--timeout=10s"})
+	config, err := testParseArgs([]string{"play", "/tmp/script.json", "--timeout=10s"})
+	assert.NoError(t, err)
 	assert.Equal(t, config.Subcommand, Play)
 	assert.Equal(t, config.Play.Timeout.Seconds(), 10.0)
+	assert.Equal(t, config.Play.ScriptFile, "/tmp/script.json")
+}
+
+func TestConfigScriptFile(t *testing.T) {
+	config, err := testParseArgs([]string{"play", "/tmp/script.json"})
+	assert.NoError(t, err)
+	assert.Equal(t, config.Subcommand, Play)
+	assert.Equal(t, config.Play.ScriptFile, "/tmp/script.json")
+}
+
+func TestConfigAcceptsOptionalExecutable(t *testing.T) {
+	config, err := testParseArgs([]string{"play", "/tmp/script.json", "/bin/ls"})
+	assert.NoError(t, err)
+	assert.Equal(t, config.Subcommand, Play)
+	assert.Equal(t, config.Play.ScriptFile, "/tmp/script.json")
+	assert.Equal(t, config.Play.Command, "/bin/ls")
+}
+
+func TestConfigRequiresScriptfile(t *testing.T) {
+	_, err := testParseArgs([]string{"play"})
+	assert.Errorf(t, err, "accepts between 1 and 2 arg(s), received 0")
 }
 
 func TestParsesRecordCommand(t *testing.T) {
 	t.Skip("not implemented")
-	config := testParseArgs([]string{"record"})
+	config, _ := testParseArgs([]string{"record"})
 	assert.Equal(t, config.Subcommand, Record)
 }
 
